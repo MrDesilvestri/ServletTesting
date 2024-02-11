577e19303effdf1399a3a228f9a3206e5f99fcf0
MrDesilvestri <124096886+MrDesilvestri@users.noreply.github.com>
2024-02-11 15:46:12 -0500

    First Update
    In this proyect i used SERVLETS to send message using JsonObjects to Anguler Front Development.
    
    Comming Soon: i will use MySQL database to send any bussines model data to Angular development.

---
+++ src/main/java/com/mycompany/pruebaservlets/HolaMundo.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.mycompany.pruebaservlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.json.simple.JSONObject;
+
+/**
+ *
+ * @author carlo
+ */
+@WebServlet(name = "HolaMundo", urlPatterns = {"/HolaMundo"})
+public class HolaMundo extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+         // Configura el tipo de contenido de la respuesta como JSON
+        response.setContentType("application/json");
+        
+        // Crea un objeto JSON
+        JSONObject jsonResponse = new JSONObject();
+        jsonResponse.put("mensaje", "¡Hola desde el servlet!");
+        
+        // Escribe el JSON en el cuerpo de la respuesta
+        response.getWriter().write(jsonResponse.toJSONString());
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
